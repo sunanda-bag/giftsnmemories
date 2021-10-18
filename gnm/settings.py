b24b2f47cd4b502c824a3bc8160ddf2557024fd2
vivek <vivek.uvsoft@gmail.com>
2021-10-18 12:47:53 -0400

    fronend

--- gnm/settings.py
+++ gnm/settings.py
@@ -130,10 +130,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+# STATIC_URL = '/static/'
+# if DEBUG:
+#     STATICFILES_DIRS = [BASE_DIR / "static",]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-if DEBUG:
-    STATICFILES_DIRS = [BASE_DIR / "static",]
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
